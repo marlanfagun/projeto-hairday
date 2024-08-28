ffed0f4f6938c9e2fe73aef4863a43f8a7c017f0
Marlan Fagundes <marlanetci@gmail.com>
2024-08-27 23:29:46 -0300

    schedule logic created

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,10 +1,13 @@
 import dayjs from "dayjs";
 
 import { openingHours } from "../../utils/opening-hours.js";
+import { hoursClick } from "./hours-click.js";
 
 const hours = document.getElementById("hours");
 
 export function hoursLoad({ date }) {
+  //Limpa a lista de horários
+  hours.innerHTML = ""
   const opening = openingHours.map((hour) => {
     //Recupera somente a hora
     const [scheduleHour] = hour.split(":");
@@ -20,12 +23,32 @@ export function hoursLoad({ date }) {
 
   //Renderiza os horários
   opening.forEach(({ hour, available }) => {
-    const li = document.createElement("li")
+    const li = document.createElement("li");
 
-    li.classList.add("hour")
-    li.classList.add(available ? "hour-available" : "hour-unavailable")
+    li.classList.add("hour");
+    li.classList.add(available ? "hour-available" : "hour-unavailable");
 
-    li.textContent = hour
-    hours.append(li)
+    li.textContent = hour;
+
+    if (hour === "9:00") {
+      hourHeaderAdd("Manhã");
+    } else if (hour === "13:00") {
+      hourHeaderAdd("Tarde");
+    } else if (hour === "18:00") {
+      hourHeaderAdd("Noite");
+    }
+
+    hours.append(li);
   });
+
+  //Adiciona o evento de click no horários disponíveis
+  hoursClick();
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li");
+  header.classList.add("hour-period");
+  header.textContent = title;
+
+  hours.append(header);
 }
--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -3,7 +3,9 @@ import { hoursLoad } from "../form/hours-load.js";
 //Seleciona o input de data
 const selectedDate = document.getElementById("date");
 
-export function scheduleDay() {
+export function schedulesDay() {
+  //Obtém a data do input
+  const date = selectedDate.value
   //Rendetiza as horas disponíveis
   hoursLoad({ date });
 }
