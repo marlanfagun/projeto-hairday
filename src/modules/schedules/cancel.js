11092f28fdeaa12a16db9950152346e40675523d
Marlan Fagundes <marlanetci@gmail.com>
2024-09-02 23:29:38 -0300

    created cancel schedule

---
+++ src/modules/schedules/cancel.js
@@ -0,0 +1,33 @@
+import { schedulesDay } from "./load.js";
+import { scheduleCancel } from "../../services/schedule-cancel.js";
+
+const periods = document.querySelectorAll(".period");
+
+//Gera evento click para cada lista (manhã, tarde e noite)
+periods.forEach((period) => {
+  //Captura o envento de clique na lista
+  period.addEventListener("click", async (event) => {
+    if (event.target.classList.contains("cancel-icon")) {
+      //Obtém a li pai do elemento clicado
+      const item = event.target.closest("li");
+
+      //Pega o id do agendamento para remover
+      const { id } = item.dataset;
+
+      //Confirma se o usuário quer cancelar
+      if (id) {
+        const isConfirm = confirm(
+          "Tem certeza que deseja cancelar o agendamento?"
+        );
+
+        if (isConfirm) {
+          //Faz a requisição na API para cancelar
+          await scheduleCancel({ id });
+
+          //Recarrega os agendamentos
+          schedulesDay();
+        }
+      }
+    }
+  });
+});
