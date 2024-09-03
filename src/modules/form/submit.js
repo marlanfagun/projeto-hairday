5a37a800069365774e31220acd626c41862c6592
Marlan Fagundes <marlanetci@gmail.com>
2024-09-02 23:00:27 -0300

    updating schedule list

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 import { scheduleNew } from "../../services/schedule-new.js";
+import { schedulesDay } from "../schedules/load.js";
 
 const form = document.querySelector("form");
 const clientName = document.getElementById("client");
@@ -43,11 +44,18 @@ form.onsubmit = async (event) => {
     //Gera um ID
     const id = new Date().getTime();
 
+    //Faz o agendamento
     await scheduleNew({
       id,
       name,
       when,
     });
+
+    //Recarrega os agendamentos
+    await schedulesDay();
+
+    //Limpa o input de nome do cliente
+    clientName.value = "";
   } catch (error) {
     alert("Não foi possível realizar o agendamento");
     console.log(error);
